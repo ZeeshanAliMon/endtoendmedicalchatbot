9496dee65658ebc67a0e39338571d50e67762e80
Ali <ali@example.com>
2026-01-03 16:59:47 +0500

    Search using chroma db is in secondapp.py and search using pinecone is in app.py, they are separated

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import PrivateAttr
 from src.helper import download_hugging_face_embeddings
-# from langchain_pinecone import Pinecone as PineconeVectorStore
+from langchain_pinecone import Pinecone as PineconeVectorStore
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
-from logging import Logger
+
 from groq import Groq
 from src.prompt import system_prompt
 from langchain_core.language_models.llms import LLM
@@ -43,8 +43,11 @@ class GroqLLM(LLM):
             model=self.model_name,
             messages=self._messages,
         )
+        self._messages.append(response.choices[0].message)
+        print(len(self._messages))
         return response.choices[0].message.content
-
+    def get_len(self):
+        return len(self._messages)
     @property
     def _identifying_params(self):
         return {"model_name": self.model_name}
@@ -57,41 +60,50 @@ embeddings = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
 index_name = "medicalbot"
-vectordb = Chroma(
-    persist_directory="./chroma_db",
-    embedding_function=embeddings
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name = index_name,
+    embedding = embeddings
 )
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+llm_store ={}
+def get_llm(session_id: str):
+    if session_id not in llm_store:
+        llm_store[session_id] = GroqLLM(system_prompt)
+        
+    return llm_store[session_id]
 
-
-retriever = vectordb.as_retriever(search_kwargs={"k": 3})
-# 3️⃣ Ask function
-def ask(query: str):
-    llm = GroqLLM(system_prompt)
+def ask(query: str,session_id:str):
+    llm = get_llm(session_id)
+    
     docs =  retriever.invoke(query)
 
     context = "\n\n".join(d.page_content for d in docs)
+    if llm.get_len() < 2:
     # print(context)
-    prompt = f"""
-
+        prompt = f"""
+if This is my first message then use both context and question.
 Context:
 {context}
 
 Question:
 {query}
-
+if this isn't my first question then iqnore the context
 """
-    return  llm.invoke(prompt)
+        return  llm.invoke(prompt)
+    else:
+        return llm.invoke(query)
 
 
 @app.post("/chat/")
 async def chat_endpoint(request: Request):
     
     data =  await request.json()
-    Logger.info(data "received")
+    # Logger.info(f"data {data}")
     chat_input = data["chatInput"]
+    session_id = data["sessionId"]
     if not chat_input:
         return {"reply": "No input provided"}
     
-    reply = ask(chat_input)
+    reply = ask(chat_input,session_id)
     
     return {"reply": reply}
--- src/prompt.py
+++ src/prompt.py
@@ -1,7 +1,12 @@
 system_prompt = (
 
     """
-You are an assistant for question_answering tasks according to the context, 
-try to answer in less than 5 sentences, if the question is irrelevant of context just say "I don't Know", nothing else
+You are an assistant for question_answering tasks according to the context,
+You will not mention about the context, user shouldnot know you are getting any context 
+If its First message of user :
+If the Query is according to context answer it ,
+If the query is not according to context just say I don't know ("just i dont know nothing else")
+If its not First message of user:
+Completely only focus on query ignore the context that has been given other than first message.
 """
 )
